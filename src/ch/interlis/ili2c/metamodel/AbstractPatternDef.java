73f45c2d476b6d73c1719607713de9c0d5eb68b3
claeis <claeis@users.noreply.github.com>
2007-05-15 06:56:08 +0000

    getTransferViewables() moved from XSDGenerator
    git-svn-id: file:///C/usr/ce/projects/sf/ili2c/gitmigration/ili2c.svn/ili2c/trunk@639 22a1f05a-e80c-4436-89a7-166f12dbcbdd

--- src/ch/interlis/ili2c/metamodel/AbstractPatternDef.java
+++ src/ch/interlis/ili2c/metamodel/AbstractPatternDef.java
@@ -1,3 +1,20 @@
+/* This file is part of the ili2c project.
+ * For more information, please see <http://www.interlis.ch>.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package ch.interlis.ili2c.metamodel;
 
 
@@ -282,6 +299,53 @@ END B;<br></pre></code>
 
 
     return result;
+  }
+  public List getTransferViewables()
+  {
+	    List result = new LinkedList ();
+	    Iterator iter = getViewables().iterator();
+	    while (iter.hasNext())
+	    {
+	      Viewable v = (Viewable)iter.next();
+	      if (!suppressViewableInTransfer (v))
+	      {
+				result.add(v);
+	      }
+	    }
+	    return result;
+  }
+  static public boolean suppressViewableInTransfer (Viewable v)
+  {
+    if (v == null)
+      return true;
+
+
+    if (v.isAbstract())
+      return true;
+
+    if(v instanceof AssociationDef){
+		AssociationDef assoc=(AssociationDef)v;
+    	if(assoc.isLightweight()){
+    		return true;
+    	}
+		if(assoc.getDerivedFrom()!=null){
+			return true;
+		}
+    }
+
+    Topic topic;
+    if ((v instanceof View) && ((topic=(Topic)v.getContainer (Topic.class)) != null)
+	    && !topic.isViewTopic())
+      return true;
+
+
+    /* STRUCTUREs do not need to be printed with their INTERLIS container,
+       but where they are used. */
+    if ((v instanceof Table) && !((Table)v).isIdentifiable())
+      return true;
+
+
+    return false;
   }
 
 
