8405fe0d9b6ee92ea65792b15e8f4922ad09af95
claeis <claeis@users.noreply.github.com>
2007-10-23 14:53:26 +0000

    - API: added SetConstraint
    git-svn-id: file:///C/usr/ce/projects/sf/ili2c/gitmigration/ili2c.svn/ili2c/trunk@688 22a1f05a-e80c-4436-89a7-166f12dbcbdd

---
+++ src/ch/interlis/ili2c/metamodel/SetConstraint.java
@@ -0,0 +1,17 @@
+ 
+package ch.interlis.ili2c.metamodel;
+
+import java.util.*;
+public class SetConstraint extends Constraint
+{
+  public SetConstraint()
+  {
+  }
+  private Evaluable preCondition=null;
+  public Evaluable getPreCondition() {
+  	return preCondition;
+  }
+  public void setPreCondition(Evaluable preCondition) {
+  	this.preCondition = preCondition;
+  }
+}
