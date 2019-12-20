9b2ca7bf28e5630dd41d58ca1f0c4c5f644d4984
claeis <ce@eisenhutinformatik.ch>
2019-12-20 12:01:13 +0100

    fix validation of BlackboxType translation (#13)

---
+++ test/java/ch/interlis/ili2c/Interlis23/translation/BlackboxType23Test.java
@@ -0,0 +1,54 @@
+package ch.interlis.ili2c.Interlis23.translation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ch.ehi.basics.logging.EhiLogger;
+import ch.interlis.ili2c.Ili2c;
+import ch.interlis.ili2c.Ili2cFailure;
+import ch.interlis.ili2c.LogCollector;
+import ch.interlis.ili2c.config.Configuration;
+import ch.interlis.ili2c.config.FileEntry;
+import ch.interlis.ili2c.config.FileEntryKind;
+import ch.interlis.ili2c.metamodel.AttributeDef;
+import ch.interlis.ili2c.metamodel.EnumerationType;
+import ch.interlis.ili2c.metamodel.Model;
+import ch.interlis.ili2c.metamodel.Table;
+import ch.interlis.ili2c.metamodel.Topic;
+import ch.interlis.ili2c.metamodel.TransferDescription;
+
+public class BlackboxType23Test {
+    @Test
+    public void blackboxTypeComplete() throws Exception {
+        LogCollector errs=new LogCollector();
+        EhiLogger.getInstance().addListener(errs);
+        Configuration ili2cConfig=new Configuration();
+        FileEntry fileEntry=new FileEntry("test/data/ili23/translation/blackboxTypeComplete.ili", FileEntryKind.ILIMODELFILE);
+        ili2cConfig.addFileEntry(fileEntry);
+        TransferDescription td=null;
+        try{
+            td=ch.interlis.ili2c.Ili2c.runCompiler(ili2cConfig);
+        }catch(Ili2cFailure ex){
+            
+        }
+        assertNotNull(td);
+        assertEquals(0,errs.getErrs().size());
+    }
+    @Test
+    public void blackboxTypeKindFail() throws Exception {
+        LogCollector errs=new LogCollector();
+        EhiLogger.getInstance().addListener(errs);
+        Configuration ili2cConfig=new Configuration();
+        FileEntry fileEntry=new FileEntry("test/data/ili23/translation/oidTypeKindFail.ili", FileEntryKind.ILIMODELFILE);
+        ili2cConfig.addFileEntry(fileEntry);
+        TransferDescription td=null;
+        try{
+            td=ch.interlis.ili2c.Ili2c.runCompiler(ili2cConfig);
+        }catch(Ili2cFailure ex){
+            
+        }
+        assertNull(td);
+        assertEquals(1,errs.getErrs().size());
+    }
+}
