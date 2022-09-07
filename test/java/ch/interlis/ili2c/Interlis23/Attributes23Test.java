4f202b4dd6cb95be25d07a4e5f17fce5d42d41b5
claeis <ce@eisenhutinformatik.ch>
2022-09-07 09:19:19 +0200

    fix test

--- test/java/ch/interlis/ili2c/Interlis23/Attributes23Test.java
+++ test/java/ch/interlis/ili2c/Interlis23/Attributes23Test.java
@@ -311,13 +311,12 @@ public class Attributes23Test {
 	}
 	
 	// This test checks if the compiler detects an extended attribute with a different type.
-	@Ignore
 	@Test
-	public void attributePathType_ExtendedAttributeWithADifferentType_Fail() {
+	public void extendedAttributeWithADifferentType_Fail() {
 		LogCollector errs=new LogCollector();
 		EhiLogger.getInstance().addListener(errs);
 		Configuration ili2cConfig=new Configuration();
-		FileEntry fileEntry=new FileEntry(TEST_OUT+"attributePathType_ExtendedAttributeWithADifferentType.ili", FileEntryKind.ILIMODELFILE);
+		FileEntry fileEntry=new FileEntry(TEST_OUT+"ExtendedAttributeWithADifferentType.ili", FileEntryKind.ILIMODELFILE);
 		ili2cConfig.addFileEntry(fileEntry);
 		TransferDescription td=null;
 		try{
@@ -328,8 +327,7 @@ public class Attributes23Test {
 		assertEquals(1,errs.getErrs().size());
 		CompilerLogEvent logEvent= (CompilerLogEvent) errs.getErrs().get(0);
 		CompilerLogEvent compilerLogEvent=(CompilerLogEvent) logEvent;
-		assertEquals(9, compilerLogEvent.getLine());
-		assertEquals("TRANSIENT mode of attribute \"attr\" requires to be the same as of base attribute.", compilerLogEvent.getRawEventMsg());
+		assertEquals("model.Coord is not extending model.Hoehe", compilerLogEvent.getRawEventMsg());
 	}
 	
 	// This test checks if the compiler accepts an extended attribute in an association.
@@ -350,9 +348,8 @@ public class Attributes23Test {
 	}
 	
 	// This test checks if the compiler accepts an extended attribute with the same enumeration domain.
-	@Ignore
 	@Test
-	public void attributeSpecialized_ExtendedAttrWithSameEnumDomain_Fail() {
+	public void attributeSpecialized_ExtendedAttrWithSameEnumDomain() {
 		LogCollector errs=new LogCollector();
 		EhiLogger.getInstance().addListener(errs);
 		Configuration ili2cConfig=new Configuration();
@@ -363,11 +360,6 @@ public class Attributes23Test {
 			td=ch.interlis.ili2c.Ili2c.runCompiler(ili2cConfig);
 		}catch(Ili2cFailure ex){
 		}
-		assertNull(td);
-		assertEquals(1,errs.getErrs().size());
-		CompilerLogEvent logEvent= (CompilerLogEvent) errs.getErrs().get(0);
-		CompilerLogEvent compilerLogEvent=(CompilerLogEvent) logEvent;
-		assertEquals(9, compilerLogEvent.getLine());
-		assertEquals("TRANSIENT mode of attribute \"attr\" requires to be the same as of base attribute.", compilerLogEvent.getRawEventMsg());
+		assertNotNull(td);
 	}
 }
