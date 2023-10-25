f5bd628cd583005c6a861d41561481543ba016c5
Dominic Burger <dominic.burger@geowerkstatt.ch>
2023-10-25 10:56:16 +0200

    Add tests for imd16 export of multi type attribute

--- test/java/ch/interlis/ili2c/generator/imd/Imd16Ili24GeneratorTest.java
+++ test/java/ch/interlis/ili2c/generator/imd/Imd16Ili24GeneratorTest.java
@@ -65,40 +65,40 @@ public class Imd16Ili24GeneratorTest {
 
     @Test
     public void genericDomain() {
-        assertTrue(metaObjects.containsKey("Simple24.GenericDomain"));
         IomObject iomObject = metaObjects.get("Simple24.GenericDomain");
+        assertNotNull(iomObject);
         String generic = iomObject.getattrvalue("Generic");
         assertEquals("true", generic);
     }
 
     @Test
     public void nonGenericDomain() {
-        assertTrue(metaObjects.containsKey("Simple24.Coord"));
         IomObject iomObject = metaObjects.get("Simple24.Coord");
+        assertNotNull(iomObject);
         String generic = iomObject.getattrvalue("Generic");
         assertEquals("false", generic);
     }
 
     @Test
     public void textType() {
-        assertTrue(metaObjects.containsKey("Simple24.TestA.ClassA1.attr1.TYPE"));
         IomObject iomObject = metaObjects.get("Simple24.TestA.ClassA1.attr1.TYPE");
+        assertNotNull(iomObject);
         String generic = iomObject.getattrvalue("Generic");
         assertEquals("false", generic);
     }
 
     @Test
     public void context() {
-        assertTrue(metaObjects.containsKey("Simple24.default"));
         IomObject contextObject = metaObjects.get("Simple24.default");
+        assertNotNull(contextObject);
         String name = contextObject.getattrvalue("Name");
         assertEquals("default", name);
     }
 
     @Test
     public void genericDef() {
-        assertTrue(metaObjects.containsKey("Simple24.default.GenericDomain"));
         IomObject genericDef = metaObjects.get("Simple24.default.GenericDomain");
+        assertNotNull(genericDef);
         IomObject context = genericDef.getattrobj("Context", 0);
         IomObject genericDomain = genericDef.getattrobj("GenericDomain", 0);
 
@@ -110,8 +110,8 @@ public class Imd16Ili24GeneratorTest {
     public void concreteForGeneric() {
         String[] concretes = { "Coord", "Coord2" };
         for (String concrete : concretes) {
-            assertTrue(metaObjects.containsKey("Simple24.default.GenericDomain." + concrete));
             IomObject concreteForGeneric = metaObjects.get("Simple24.default.GenericDomain." + concrete);
+            assertNotNull(concreteForGeneric);
             IomObject genericDef = concreteForGeneric.getattrobj("GenericDef", 0);
             IomObject concreteDomain = concreteForGeneric.getattrobj("ConcreteDomain", 0);
 
@@ -122,11 +122,11 @@ public class Imd16Ili24GeneratorTest {
 
     @Test
     public void modelAttributes24() {
-        assertTrue(metaObjects.containsKey("Simple24"));
         IomObject simpleModel = metaObjects.get("Simple24");
+        assertNotNull(simpleModel);
 
-        assertTrue(metaObjects.containsKey("ModelAttributes24"));
         IomObject attributesModel = metaObjects.get("ModelAttributes24");
+        assertNotNull(attributesModel);
 
         assertNotEquals("true", simpleModel.getattrvalue("NoIncrementalTransfer"));
         assertEquals("true", attributesModel.getattrvalue("NoIncrementalTransfer"));
@@ -136,5 +136,38 @@ public class Imd16Ili24GeneratorTest {
 
         assertNull(simpleModel.getattrvalue("xmlns"));
         assertEquals("http://www.interlis.ch/custom/namespace", attributesModel.getattrvalue("xmlns"));
+    }
+
+    @Test
+    public void multiCoordType() {
+        IomObject coordType = metaObjects.get("Simple24.Coord");
+        assertNotNull(coordType);
+        assertEquals("false", coordType.getattrvalue("Multi"));
+
+        IomObject multiCoordType = metaObjects.get("Simple24.MultiCoord");
+        assertNotNull(multiCoordType);
+        assertEquals("true", multiCoordType.getattrvalue("Multi"));
+    }
+
+    @Test
+    public void multiLineType() {
+        IomObject lineType = metaObjects.get("Simple24.Line");
+        assertNotNull(lineType);
+        assertEquals("false", lineType.getattrvalue("Multi"));
+
+        IomObject multiLineType = metaObjects.get("Simple24.MultiLine");
+        assertNotNull(multiLineType);
+        assertEquals("true", multiLineType.getattrvalue("Multi"));
+    }
+
+    @Test
+    public void multiSurfaceType() {
+        IomObject surfaceType = metaObjects.get("Simple24.Surface");
+        assertNotNull(surfaceType);
+        assertEquals("false", surfaceType.getattrvalue("Multi"));
+
+        IomObject multiSurfaceType = metaObjects.get("Simple24.MultiSurface");
+        assertNotNull(multiSurfaceType);
+        assertEquals("true", multiSurfaceType.getattrvalue("Multi"));
     }
 }
