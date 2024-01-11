5fc9dc7e695ac0c013a0b8dfe7cd2d9feea13910
claeis <ce@eisenhutinformatik.ch>
2024-01-11 09:53:06 +0100

    fix validation of #true

--- ili2c-core/src/main/java/ch/interlis/ili2c/metamodel/Constant.java
+++ ili2c-core/src/main/java/ch/interlis/ili2c/metamodel/Constant.java
@@ -417,6 +417,9 @@ public abstract class Constant extends Evaluable
     }
     @Override
     public boolean isLogical() {
+        return isBoolean();
+    }
+    private boolean isBoolean() {
         if(value.length==1) {
             if(value[0].equals("true") || value[0].equals("false")) {
                 return true;
@@ -427,6 +430,9 @@ public abstract class Constant extends Evaluable
     @Override
     public Type getType() {
         if(type==null) {
+            if(isBoolean()) {
+                return PredefinedModel.getInstance().BOOLEAN.getType();
+            }
             return new EnumerationType();
         }
         return type;
@@ -434,6 +440,16 @@ public abstract class Constant extends Evaluable
     public void setType(Type type)
     {
         this.type=type;
+    }
+    @Override
+    public Element getSourceOfType() {
+        if(sourceOfType==null && isBoolean()) {
+            return PredefinedModel.getInstance().BOOLEAN;
+        }
+        return sourceOfType;
+    }
+    public void setSourceOfType(Element sourceOfType) {
+        this.sourceOfType = sourceOfType;
     }
 
 
@@ -519,13 +535,6 @@ public abstract class Constant extends Evaluable
             return ((EnumTreeValueType) type).getValues();
         }
         throw new IllegalStateException("unexpected type "+type.getScopedName());
-    }
-    @Override
-    public Element getSourceOfType() {
-        return sourceOfType;
-    }
-    public void setSourceOfType(Element sourceOfType) {
-        this.sourceOfType = sourceOfType;
     }
   } /* Constant.Enumeration */
 
