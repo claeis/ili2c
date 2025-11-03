684d332066a734d45c947fc22ba6f7ea9308ff3d
claeis <ce@eisenhutinformatik.ch>
2025-11-03 14:24:54 +0100

    add line number to modelscan message (#147)

--- test/java/ch/interlis/ili2c/Interlis23/IliDoc23Test.java
+++ test/java/ch/interlis/ili2c/Interlis23/IliDoc23Test.java
@@ -16,12 +16,33 @@ import ch.interlis.ili2c.metamodel.TransferDescription;
 public class IliDoc23Test {
 
     @Test
-    public void unterminatedDocComment_Fail() {
+    public void unterminatedDocComment_AutoComplete_Fail() {
         LogCollector errs=new LogCollector();
         EhiLogger.getInstance().addListener(errs);
         Configuration ili2cConfig=new Configuration();
         FileEntry fileEntry=new FileEntry("test/data/ili23/comments/unterminatedDocComment.ili", FileEntryKind.ILIMODELFILE);
         ili2cConfig.addFileEntry(fileEntry);
+        ili2cConfig.setAutoCompleteModelList(true);
+        TransferDescription td=null;
+        try{
+            td=ch.interlis.ili2c.Ili2c.runCompiler(ili2cConfig);
+            fail("compiler should fail because of unterminated comment");
+        }catch(Ili2cFailure ex){
+        }
+        assertNull(td);
+        assertTrue(errs.getErrs().size()>0);
+        CompilerLogEvent logEvent=(CompilerLogEvent)errs.getAdaptions().get(0);
+        assertEquals(4, logEvent.getLine());
+        assertTrue(logEvent.getRawEventMsg().contains("Unterminated comment"));
+    }
+    @Test
+    public void unterminatedDocComment_NoAutoComplete_Fail() {
+        LogCollector errs=new LogCollector();
+        EhiLogger.getInstance().addListener(errs);
+        Configuration ili2cConfig=new Configuration();
+        FileEntry fileEntry=new FileEntry("test/data/ili23/comments/unterminatedDocComment.ili", FileEntryKind.ILIMODELFILE);
+        ili2cConfig.addFileEntry(fileEntry);
+        ili2cConfig.setAutoCompleteModelList(false);
         TransferDescription td=null;
         try{
             td=ch.interlis.ili2c.Ili2c.runCompiler(ili2cConfig);
